3de1973fcb4d34d44527a08ee711633e4947e068
kennytat <kennytat.app@gmail.com>
2021-06-09 11:58:04 +0700

    finish restructure prisma db, not yet graphql

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,61 +2,104 @@ import { PrismaClient, Prisma } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const userData: Prisma.UserCreateInput[] = [
+const rootData: Prisma.RootCreateInput[] = [
   {
-    name: 'Alice',
-    email: 'alice@prisma.io',
-    posts: {
+    name: 'root',
+    qm: 'Qm00',
+    categories: {
       create: [
         {
-          title: 'Join the Prisma Slack',
-          content: 'https://slack.prisma.io',
-          published: true,
+          name: 'videoCategory',
+          qm: 'Qm1',
+          classify: {
+            create: [
+              {
+                name: 'videoClassification',
+                qm: 'Qm11',
+                topic: {
+                  create: [
+                    {
+                      name: 'videoTopic',
+                      qm: 'Qm111',
+                      content: {
+                        create: [
+                          {
+                            name: 'video1',
+                            qm: 'Qm1111',
+                            duration: 520,
+                            size: 123,
+                            thumb: '/this is video1 thumbnail url',
+                            isvideo: true
+                          },
+                          {
+                            name: 'video2',
+                            qm: 'Qm1112',
+                            duration: 123123,
+                            size: 123312313,
+                            thumb: '/this is video2 thumbnail url',
+                            isvideo: true
+                          } 
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
+        },
+        {
+          name: 'audioCategory',
+          qm: 'Qm2',
+          classify: {
+            create: [
+              {
+                name: 'audioClassification',
+                qm: 'Qm21',
+                topic: {
+                  create: [
+                    {
+                      name: 'audioTopic',
+                      qm: 'Qm211',
+                      content: {
+                        create: [
+                          {
+                            name: 'audio1',
+                            qm: 'Qm2111',
+                            duration: 520,
+                            size: 123,
+                            thumb: '/this is audio thumbnail url',
+                            isvideo: true
+                          },
+                          {
+                            name: 'audio2',
+                            qm: 'Qm2112',
+                            duration: 520,
+                            size: 123,
+                            thumb: '/this is audio thumbnail url',
+                            isvideo: true
+                          }
+                        ]
+                      }
+                    }
+                  ]
+                }
+              }
+            ]
+          }
         },
       ],
     },
-  },
-  {
-    name: 'Nilu',
-    email: 'nilu@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Follow Prisma on Twitter',
-          content: 'https://www.twitter.com/prisma',
-          published: true,
-          viewCount: 42,
-        },
-      ],
-    },
-  },
-  {
-    name: 'Mahmoud',
-    email: 'mahmoud@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Ask a question about Prisma on GitHub',
-          content: 'https://www.github.com/prisma/prisma/discussions',
-          published: true,
-          viewCount: 128,
-        },
-        {
-          title: 'Prisma on YouTube',
-          content: 'https://pris.ly/youtube',
-        },
-      ],
-    },
-  },
+  }
 ]
 
 async function main() {
   console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.user.create({
+  for (const u of rootData) {
+    const root = await prisma.root.create({
       data: u,
     })
-    console.log(`Created user with id: ${user.id}`)
+    console.log(`Created user with id: ${root.id}`)
   }
   console.log(`Seeding finished.`)
 }
